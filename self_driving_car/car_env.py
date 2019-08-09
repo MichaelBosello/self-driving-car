3f5cade447ff4c3967ece6460c4b642a931bc304
Michael Bosello <michael.bosello@studio.unibo.it>
2019-08-09 19:33:54 +0200

    Enanche car behaviour during crash

--- self_driving_car/car_env.py
+++ self_driving_car/car_env.py
@@ -15,7 +15,7 @@ from sensor.car_sensor import CarSensor
 #   Step: An Environment step (e.g. covers 4 frames)
 #
 
-CAR_INSTANCE = 'hjduino'
+CAR_INSTANCE = 'xiaor'
 
 if CAR_INSTANCE == 'picar':
   from camera.pi_camera import CarCamera
@@ -32,6 +32,11 @@ class CarEnv:
         self.camera.start_recording()
         self.motor = CarMotor(CAR_INSTANCE)
         self.sensor = CarSensor(CAR_INSTANCE)
+
+        def stop_callback(self):
+          self.motor.stop()
+
+        self.sensor.add_callback_to_crash(stop_callback)
 
         self.actionSet = [0, 1, 2, 3, 4]
         self.gameNumber = 0
@@ -52,37 +57,38 @@ class CarEnv:
         self.episode_frame_number +=1
 
         if self.sensor.front_crash() or self.sensor.rx_front_crash() or self.sensor.lx_front_crash() or self.sensor.rx_back_crash() or self.sensor.lx_back_crash():
-          self.car_to_safe()
+          while self.sensor.front_crash() or self.sensor.rx_front_crash() or self.sensor.lx_front_crash() or self.sensor.rx_back_crash() or self.sensor.lx_back_crash():
+            self.car_to_safe()
           reward = -1
           self.isTerminal = True
-          self.state = self.state.stateByAddingScreen(self.camera.capture_as_rgb_array(), self.frame_number)
+          self.state = self.state.stateByAddingScreen(self.camera.capture_as_rgb_array_bottom_half(), self.frame_number)
           return reward, self.state, self.isTerminal
 
         reward = 0
         if action == 0:
           self.motor.forward()
-          reward = 0.8
+          reward = 0.6
         elif action == 1:
           self.motor.backward()
           reward = -0.4
         elif action == 2:
           self.motor.right()
-          reward = 0.6
+          reward = 0.2
         elif action == 3:
           self.motor.left()
-          reward = 0.6
+          reward = 0.2
         elif action == 4:
           self.motor.stop()
         else:
           raise ValueError('`action` should be between 0 and 4.')
         
-        prevScreenRGB = self.camera.capture_as_rgb_array()
-        screenRGB = self.camera.capture_as_rgb_array()
+        prevScreenRGB = self.camera.capture_as_rgb_array_bottom_half()
+        screenRGB = self.camera.capture_as_rgb_array_bottom_half()
 
-        if self.sensor.rx_above_line():
-          reward += 0.1
-        if self.sensor.lx_above_line():
-          reward += 0.1
+        #if self.sensor.rx_above_line():
+        #  reward += 0.02
+        #if self.sensor.lx_above_line():
+        #  reward += 0.02
 
         maxedScreen = np.maximum(screenRGB, prevScreenRGB)
         self.state = self.state.stateByAddingScreen(maxedScreen, self.frame_number)
@@ -93,7 +99,7 @@ class CarEnv:
         if self.isTerminal:
             self.gameNumber += 1
             self.isTerminal = False
-        self.state = State().stateByAddingScreen(self.camera.capture_as_rgb_array(), self.frame_number)
+        self.state = State().stateByAddingScreen(self.camera.capture_as_rgb_array_bottom_half(), self.frame_number)
         self.gameScore = 0
         self.episodeStepNumber = 0
         self.episode_frame_number = 0
@@ -101,22 +107,38 @@ class CarEnv:
 
     def car_to_safe(self):
         self.motor.stop()
+        straighten = None
+        # front crash
         if self.sensor.front_crash() or self.sensor.rx_front_crash() or self.sensor.lx_front_crash():
+          if not self.sensor.front_crash():
+            if self.sensor.rx_front_crash() and not self.sensor.lx_front_crash():
+              straighten = "lx"
+            if self.sensor.lx_front_crash() and not self.sensor.rx_front_crash():
+              straighten = "rx"
           self.motor.backward()
           while self.sensor.front_crash() or self.sensor.rx_front_crash() or self.sensor.lx_front_crash():
             if self.sensor.rx_back_crash() or self.sensor.lx_back_crash():
               break
-            time.sleep(0.01)
-          self.motor.stop()
-          self.car_to_safe()
+            time.sleep(0.001)
+        # back crash
         if self.sensor.rx_back_crash() or self.sensor.lx_back_crash():
+          if self.sensor.rx_back_crash() and not self.sensor.lx_back_crash():
+            straighten = "lx"
+          if self.sensor.lx_back_crash() and not self.sensor.rx_back_crash():
+              straighten = "rx"
           self.motor.forward()
           while self.sensor.rx_back_crash() or self.sensor.lx_back_crash():
             if self.sensor.front_crash() or self.sensor.rx_front_crash() or self.sensor.lx_front_crash():
               break
-            time.sleep(0.01)
-          self.motor.stop()
-          self.car_to_safe()
+            time.sleep(0.001)
+        # straighten
+        if(straighten == "rx"):
+          self.motor.right()
+          time.sleep(0.08)
+        if(straighten == "lx"):
+          self.motor.left()
+          time.sleep(0.08)
+        self.motor.stop()
 
     def stop(self):
       self.motor.stop()
