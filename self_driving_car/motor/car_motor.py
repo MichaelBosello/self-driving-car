0dc03075cad89d2efa869194806ed87a94a6367e
Michael Bosello <michael.bosello@studio.unibo.it>
2019-08-09 11:27:08 +0200

    Refactor motors and sensors packages

--- self_driving_car/motor/car_motor.py
+++ self_driving_car/motor/car_motor.py
@@ -1,9 +1,9 @@
 def CarMotor(car_type):
     if car_type == 'hjduino':
-      from motor.car_specific_motor.car_motor_hjduino_jetson import CarMotorHJduino
+      from motor.car_specific_motor.hjduino.car_motor_hjduino_jetson import CarMotorHJduino
       return CarMotorHJduino()
     if car_type == 'xiaor':
-      from motor.car_specific_motor.car_motor_xiaor_jetson import CarMotorXiaoR
+      from motor.car_specific_motor.xiaor.car_motor_xiaor_jetson import CarMotorXiaoR
       return CarMotorXiaoR()
     if car_type == 'picar':
       from motor.car_specific_motor.picar.car_motor_picar import CarMotorPicar
--- self_driving_car/sensor/car_sensor.py
+++ self_driving_car/sensor/car_sensor.py
@@ -1,10 +1,10 @@
 def CarSensor(car_type):
     if car_type == 'hjduino':
-      from sensor.car_specific_sensor.car_sensor_hjduino_jetson import CarSensorHJduino
+      from sensor.car_specific_sensor.hjduino.car_sensor_hjduino_jetson import CarSensorHJduino
       return CarSensorHJduino()
     if car_type == 'xiaor':
-      from sensor.car_specific_sensor.car_sensor_xiaor_jetson import CarSensorXiaoR
+      from sensor.car_specific_sensor.xiaor.car_sensor_xiaor_jetson import CarSensorXiaoR
       return CarSensorXiaoR()
     if car_type == 'picar':
-      from sensor.car_specific_sensor.car_sensor_picar import CarSensorPicar
+      from sensor.car_specific_sensor.picar.car_sensor_picar_raspberry import CarSensorPicar
       return CarSensorPicar()
