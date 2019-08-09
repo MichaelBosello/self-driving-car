678b9541b8fef3626d65636276e64003390f48fc
Michael Bosello <michael.bosello@studio.unibo.it>
2019-08-09 19:32:23 +0200

    Add possibility to safely quit application, params update

--- self_driving_car/rl_car_driver.py
+++ self_driving_car/rl_car_driver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #
+from threading import Thread
 import sys
 import numpy as np
 import os
@@ -7,22 +8,23 @@ import random
 import replay
 import time
 import argparse
+
 import dqn
 from car_env import CarEnv
 from state import State
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--train-epoch-steps", type=int, default=250000, help="how many steps (=4 frames) to run during a training epoch (approx -- will finish current game)")
-parser.add_argument("--eval-epoch-steps", type=int, default=125000, help="how many steps (=4 frames) to run during an eval epoch (approx -- will finish current game)")
+parser.add_argument("--train-epoch-steps", type=int, default=25000, help="how many steps (=4 frames) to run during a training epoch (approx -- will finish current game)")
+parser.add_argument("--eval-epoch-steps", type=int, default=12500, help="how many steps (=4 frames) to run during an eval epoch (approx -- will finish current game)")
 parser.add_argument("--replay-capacity", type=int, default=1000000, help="how many states to store for future training")
 parser.add_argument("--prioritized-replay", action='store_true', help="Prioritize interesting states when training (e.g. terminal or non zero rewards)")
 parser.add_argument("--compress-replay", action='store_true', help="if set replay memory will be compressed with blosc, allowing much larger replay capacity")
 parser.add_argument("--normalize-weights", action='store_true', default=True, help="if set weights/biases are normalized like torch, with std scaled by fan in to the node")
 parser.add_argument("--screen-capture-freq", type=int, default=250, help="record screens for a game this often")
-parser.add_argument("--save-model-freq", type=int, default=10000, help="save the model once per 10000 training sessions")
-parser.add_argument("--observation-steps", type=int, default=50000, help="train only after this many stesp (=4 frames)")
+parser.add_argument("--save-model-freq", type=int, default=100, help="save the model once per 10000 training sessions")
+parser.add_argument("--observation-steps", type=int, default=5000, help="train only after this many stesp (=4 frames)")
 parser.add_argument("--learning-rate", type=float, default=0.00025, help="learning rate (step size for optimization algo)")
-parser.add_argument("--target-model-update-freq", type=int, default=10000, help="how often (in steps) to update the target model.  Note nature paper says this is in 'number of parameter updates' but their code says steps. see tinyurl.com/hokp4y8")
+parser.add_argument("--target-model-update-freq", type=int, default=1000, help="how often (in steps) to update the target model.  Note nature paper says this is in 'number of parameter updates' but their code says steps. see tinyurl.com/hokp4y8")
 parser.add_argument("--model", help="tensorflow model checkpoint file to initialize from")
 args = parser.parse_args()
 
@@ -39,19 +41,31 @@ dqn = dqn.DeepQNetwork(environment.getNumActions(), baseOutputDir, args)
 
 replayMemory = replay.ReplayMemory(args)
 
+stop = False
+
+def stop_handler():
+  global stop
+  while not stop:
+    user_input = input()
+    if user_input == 'q':
+      print("Stopping...")
+      stop = True
+process = Thread(target=stop_handler)
+process.start()
+
 def runEpoch(minEpochSteps, evalWithEpsilon=None):
     stepStart = environment.getStepNumber()
     isTraining = True if evalWithEpsilon is None else False
     startGameNumber = environment.getGameNumber()
     epochTotalScore = 0
 
-    while environment.getStepNumber() - stepStart < minEpochSteps:
+    while environment.getStepNumber() - stepStart < minEpochSteps and not stop:
     
         startTime = lastLogTime = time.time()
         stateReward = 0
         state = None
         
-        while not environment.isGameOver():
+        while not environment.isGameOver() and not stop:
             print("...")
             # Choose next action
             if evalWithEpsilon is None:
@@ -62,7 +76,7 @@ def runEpoch(minEpochSteps, evalWithEpsilon=None):
             if state is None or random.random() > (1 - epsilon):
                 action = random.randrange(environment.getNumActions())
             else:
-                screens = np.reshape(state.getScreens(), (1, 84, 84, 4))
+                screens = np.reshape(state.getScreens(), (1, State.IMAGE_SIZE, State.IMAGE_SIZE, 4))
                 action = dqn.inference(screens)
 
             # Make the move
@@ -93,11 +107,14 @@ def runEpoch(minEpochSteps, evalWithEpsilon=None):
         environment.resetGame()
     
     # return the average score
+    if environment.getGameNumber() - startGameNumber == 0:
+        return 0
     return epochTotalScore / (environment.getGameNumber() - startGameNumber)
 
 
-while True:
+while not stop:
     aveScore = runEpoch(args.train_epoch_steps) #train
     print('Average training score: %d' % (aveScore))
     aveScore = runEpoch(args.eval_epoch_steps, evalWithEpsilon=.05) #eval
     print('Average eval score: %d' % (aveScore))
+    environment.stop()
