9ad888add745eeeb84b0499bd9a0f866f01b041f
Michael Bosello <michael.bosello@studio.unibo.it>
2019-08-19 22:24:21 +0200

    Fix epsilon, add epsilon decay. Add beep when epoch finish. Params update

--- self_driving_car/rl_car_driver.py
+++ self_driving_car/rl_car_driver.py
@@ -22,11 +22,14 @@ parser.add_argument("--compress-replay", action='store_true', help="if set repla
 parser.add_argument("--normalize-weights", action='store_true', help="if set weights/biases are normalized like torch, with std scaled by fan in to the node")
 parser.add_argument("--save-model-freq", type=int, default=2500, help="save the model once per X training sessions")
 parser.add_argument("--observation-steps", type=int, default=200, help="train only after this many stesp (=X frames)")
-parser.add_argument("--learning-rate", type=float, default=0.002, help="learning rate (step size for optimization algo)")
+parser.add_argument("--learning-rate", type=float, default=0.00025, help="learning rate (step size for optimization algo)")
 parser.add_argument("--gamma", type=float, default=0.999, help="gamma [0, 1] is the discount factor. It determines the importance of future rewards. A factor of 0 will make the agent consider only immediate reward, a factor approaching 1 will make it strive for a long-term high reward")
 parser.add_argument("--target-model-update-freq", type=int, default=300, help="how often (in steps) to update the target model.  Note nature paper says this is in 'number of parameter updates' but their code says steps. see tinyurl.com/hokp4y8")
 parser.add_argument("--model", help="tensorflow model checkpoint file to initialize from")
 parser.add_argument("--frame", type=int, default=1, help="frame per step")
+parser.add_argument("--epsilon", type=float, default=1, help="]0, 1]for epsilon greedy train")
+parser.add_argument("--epsilon-decay", type=float, default=0.99992, help="]0, 1] every step epsilon = epsilon * decay, in order to decrease constantly")
+parser.add_argument("--epsilon-min", type=float, default=0.1, help="epsilon with decay doesn't fall below epsilon min")
 args = parser.parse_args()
 
 print('Arguments: ', (args))
@@ -53,7 +56,10 @@ def stop_handler():
 process = Thread(target=stop_handler)
 process.start()
 
+train_epsilon = args.epsilon#don't want to reset epsilon between epoch
+
 def runEpoch(minEpochSteps, evalWithEpsilon=None):
+    global train_epsilon
     stepStart = environment.getStepNumber()
     isTraining = True if evalWithEpsilon is None else False
     startGameNumber = environment.getGameNumber()
@@ -68,11 +74,16 @@ def runEpoch(minEpochSteps, evalWithEpsilon=None):
         while not environment.isGameOver() and not stop:
             # Choose next action
             if evalWithEpsilon is None:
-                epsilon = 0.1#max(.1, 1.0 - 0.9 * environment.getStepNumber() / 1e6)
+                epsilon = train_epsilon
             else:
                 epsilon = evalWithEpsilon
 
-            if state is None or random.random() > (epsilon):
+            if train_epsilon > args.epsilon_min:
+                train_epsilon = train_epsilon * args.epsilon_decay
+                if train_epsilon < args.epsilon_min:
+                    train_epsilon = args.epsilon_min
+
+            if state is None or random.random() < (epsilon):
                 action = random.randrange(environment.getNumActions())
             else:
                 screens = np.reshape(state.getScreens(), (1, State.IMAGE_SIZE, State.IMAGE_SIZE, args.frame))
@@ -102,6 +113,8 @@ def runEpoch(minEpochSteps, evalWithEpsilon=None):
         print('%s %d ended with score: %d (%d frames in %fs for %d fps)' %
             ('Episode' if isTraining else 'Eval', environment.getGameNumber(), environment.getGameScore(),
             environment.getEpisodeFrameNumber(), episodeTime, environment.getEpisodeFrameNumber() / episodeTime))
+        if isTraining:
+          print("epsilon " + str(train_epsilon))
         epochTotalScore += environment.getGameScore()
         environment.resetGame()
     
@@ -114,6 +127,14 @@ def runEpoch(minEpochSteps, evalWithEpsilon=None):
 while not stop:
     aveScore = runEpoch(args.train_epoch_steps) #train
     print('Average training score: %d' % (aveScore))
+    print('\a')
+    print('\a')
+    print('\a')
+    print('\a')
     aveScore = runEpoch(args.eval_epoch_steps, evalWithEpsilon=.05) #eval
     print('Average eval score: %d' % (aveScore))
+    print('\a')
+    print('\a')
+    print('\a')
+    print('\a')
 environment.stop()
